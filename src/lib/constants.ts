57ca92d120614496ff39f5d76328fa3365122de1
Daksh <djain00009@gmail.com>
2025-07-13 17:06:49 +0000

    remove 3d

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,6 @@ export const NAV_LINKS = [
   { name: 'Projects', href: '/projects' },
   { name: 'Gallery', href: '/gallery' },
   { name: 'Blog', href: '/blog' },
-  { name: '3D Design', href: '/3d-view' },
   { name: 'Optimizer', href: '/optimizer' },
   { name: 'Contact', href: '/contact' },
 ];
