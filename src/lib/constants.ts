e77321b5c712e03313a4f1f13c6912c2f2d69e6c
Daksh <djain00009@gmail.com>
2025-07-13 17:12:34 +0000

    remove home section

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,4 @@
 export const NAV_LINKS = [
-  { name: 'Home', href: '/' },
   { name: 'About', href: '/about' },
   { name: 'Projects', href: '/projects' },
   { name: 'Work Experience', href: '/work-experience' },
